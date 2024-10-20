ccef54866ecfa31d0b863de155681bd577d00be6
Sean O'Leary <sean@o-leary.co.nz>
2024-10-20 15:17:12 +1300

    Update whisper.py
    Changed port to coexist with openedai-speech

--- whisper.py
+++ whisper.py
@@ -134,7 +134,7 @@ def parse_args(argv=None):
     parser.add_argument('-m', '--model', action='store', default="openai/whisper-tiny.en", help="The model to use for transcription. Ex. distil-whisper/distil-medium.en")
     parser.add_argument('-d', '--device', action='store', default="auto", help="Set the torch device for the model. Ex. cuda:1")
     parser.add_argument('-t', '--dtype', action='store', default="auto", help="Set the torch data type for processing (float32, float16, bfloat16)")
-    parser.add_argument('-P', '--port', action='store', default=8000, type=int, help="Server tcp port")
+    parser.add_argument('-P', '--port', action='store', default=8001, type=int, help="Server tcp port")
     parser.add_argument('-H', '--host', action='store', default='localhost', help="Host to listen on, Ex. 0.0.0.0")
     parser.add_argument('--preload', action='store_true', help="Preload model and exit.")
 
